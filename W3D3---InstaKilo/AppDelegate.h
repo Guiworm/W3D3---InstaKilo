e01c0010a35566ea5fe94231944a4bc377ebd4d9
guiworm <dylan.mccrindle@gmail.com>
2016-11-16 11:18:14 -0500

    Initial Commit

---
+++ W3D3---InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W3D3---InstaKilo
+//
+//  Created by Dylan McCrindle on 2016-11-16.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W3D3---InstaKilo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  W3D3---InstaKilo
+//
+//  Created by Dylan McCrindle on 2016-11-16.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
